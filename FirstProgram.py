59956d47e5d0ae60d13feee8dc9ba7ebbfea2369
jeevanahuja <jeevanahuja5@gmail.com>
2025-11-27 20:08:38 +0500

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,17 @@
+# print("Jeevan Ahuja")
+# print("Age 19")
+
+# a=int(input("Enter Number :"))
+# b=int(input("Enter Number :"))
+# print("Sum Is",a+b)
+
+# a=float(input("Enter Number :"))
+# print("Area Is",a*a)
+
+# a=float(input("Enter Number : "))
+# b=float(input("Enter Number : "))
+# print("Average Is ",(a+b)/2)
+
+# a=int(input("Enter First Number : "))
+# b=int(input("Enter Second Number : "))
+# print(a>b)
